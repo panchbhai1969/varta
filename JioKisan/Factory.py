ff3dffece5113c86927f9a3f25778b10cfdf3f38
Akhil <akhilr@iitbhilai.ac.in>
2019-02-23 00:47:14 +0530

    database populator

--- JioKisan/Factory.py
+++ JioKisan/Factory.py
@@ -1,20 +1,21 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sihtrucks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JioKisan.settings')
 
 import django
 django.setup()
 
 import random
-from KdramaRocks.models import Driver
+from trucks.models import Driver, Delivery
 from faker import Faker
-from faker.providers import geo
+from faker.providers import geo, date_time
 
 
 fakegen = Faker()
 fakegen.add_provider(geo)
+fakegen.add_provider(date_time)
 
-def populate(N=5):
+def populateDriver(N=5):
     for entry in range(N):
         name = fakegen.name()
         currentPositionLongitude = fakegen.longitude()
@@ -33,20 +34,29 @@ def populate(N=5):
                                               truckCapacity=truckCapacity
                                               )[0]
 
+def populateDelivery(N=5):
+    for entry in range(N):
+        pickupLocationLongitude = fakegen.longitude()
+        pickupLocationLatitude = fakegen.latitude()
+        dropLocationLongitude = fakegen.longitude()
+        dropLocationLatitude = fakegen.latitude()
+        pickupDate = fakegen.date_time_this_month(before_now=True, after_now=False, tzinfo=None)
+        dropDate = fakegen.date_time_this_month(before_now=False, after_now=True, tzinfo=None)
+        weight = random.randint(0,2000)
+        choices = ["PENDING", "TRANSIT", "COMPLETED"]
+        index = random.randint(0,2)
+        chosen = choices[index]
+
+        delivery = Delivery.objects.get_or_create(pickupLocationLongitude=pickupLocationLongitude,
+                                                  pickupLocationLatitude=pickupLocationLatitude,
+                                                  dropLocationLongitude=dropLocationLongitude,
+                                                  dropLocationLatitude=dropLocationLatitude,
+                                                  pickupDate=pickupDate,
+                                                  dropDate=dropDate,
+                                                  weight=weight,
+                                                  status=chosen)
+
 
 if __name__=='__main__':
-    populate(6)
-
-# class UserFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = Driver
-
-#     id = factory.Faker('id')
-#     name = factory.Faker('name')
-#     currentPositionLongitude = factory.Faker('currentPositionLongitude')
-#     currentPositionLatitude = factory.Faker('currentPositionLatitude')
-#     homeAddressLongitude = factory.Faker('homeAddressLongitude')
-#     homeAddressLatitude = factory.Faker('homeAddressLatitude')
-#     truckCapacity = factory.Faker('truckCapacity')
-#     hired = factory.Faker('hired')
-#     currentCapacity = factory.Faker('currentCapacity')
+    populateDriver(6)
+    populateDelivery(6)

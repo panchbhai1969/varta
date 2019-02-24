5e390bd72aa84873b9135986fe1c07cbe42a1a3a
panchbhai1969 <panchbhai1969@gmail.com>
2019-02-25 00:43:25 +0530

    Repository for only templates

--- JioKisan/JioKisan/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class UserRequest(forms.Form):
-    msg=forms.CharField(label='your message',max_length=200)
--- JioKisan/trucks/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Delivery, Driver
-# Register your models here.
-
-admin.site.register(Driver)
-admin.site.register(Delivery)
-

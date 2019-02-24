18b26247fd78246dc51b3e646f45fcb1355c6026
Rishabh Poddar <rishabh.poddar0207@gmail.com>
2019-02-25 02:00:25 +0530

    Registration working

--- JioKisan/JioKisan/models.py
+++ JioKisan/JioKisan/models.py
@@ -24,15 +24,15 @@ class User_reg(models.Model):
     name = models.CharField(max_length=40)
     phone_number = models.CharField(max_length=40,unique=True)
     role = models.IntegerField(default=0)
-    address = models.CharField(max_length=100,)
+    address = models.CharField(max_length=100)
     PAN = models.CharField(max_length=10,primary_key=True)
-    licence_number=models.CharField(max_length=20)
-    vehicle_number=models.CharField(max_length=20)
-    vehicle_model=models.IntegerField()
-    vehicle_capacity=models.IntegerField()
-    organisation_name=models.CharField(max_length=80)
-    bank_account_number=models.CharField(max_length=20)
-    GST_number=models.CharField(max_length=20)
+    licence_number=models.CharField(max_length=20,default=None, blank=True, null=True)
+    vehicle_number=models.CharField(max_length=20,default=None, blank=True, null=True)
+    vehicle_model=models.IntegerField(default=None, blank=True, null=True)
+    vehicle_capacity=models.IntegerField(default=None, blank=True, null=True)
+    organisation_name=models.CharField(max_length=80,default=None, blank=True, null=True)
+    bank_account_number=models.CharField(max_length=20,default=None, blank=True, null=True)
+    GST_number=models.CharField(max_length=20,default=None, blank=True, null=True)
     isVerified=models.BooleanField()
 
 class FarmEntity(models.Model):
@@ -81,53 +81,58 @@ def AddFarmEntity(mname,mMSP,mMeasured_in):
 
 def RegisterUser(mdict):
     client = base.Client(('localhost', 11211))
-    try:
-        nu=None
-        exists=User_reg.objects.filter(phone_number=mdict['phone_number']).count()
-        if exists!=0:
-            nu=User_reg.objects.get(phone_number=mdict['phone_number'])
-            if nu.isVerified:
-                return 'fail phone already exists'
-        exists=User_reg.objects.filter(PAN=mdict['PAN']).count()
-        if exists!=0:
-            nu=User_reg.objects.get(PAN=mdict['PAN'])
-            if nu.isVerified:
-                return 'fail user already exists'
-        if nu is None:
-            nu=User_reg()
-        nu.name=mdict['name']
-        nu.phone_number=mdict['phone_number']
-        nu.role=int(mdict['role'])
-        nu.PAN=mdict['PAN']
-        if nu.role==1:# is Farmer
-            pass
-        elif nu.role==2:
-            nu.vehicle_model=int(mdict['vehicle_model'])
-            nu.vehicle_capacity=int(mdict['vehicle_capacity'])
-            nu.vehicle_number=mdict['vehicle_number']
-            nu.licence_number=mdict['licence_number']
-        elif nu.role==3:
-            nu.organisation_name=mdict['organisation_name']
-        elif nu.role==4:
-            nu.bank_account_number=mdict['bank_account_number']
-            nu.GST_number=mdict['GST_number']
-        else:
-            raise ('invalid role')
-            nu.isVerified=False
-        otp=str(uuid.uuid1().int%1000000)
-        print('otp is '+otp)
-        client.set(mdict['PAN'],otp,30)        
-        return 'success'
-    except:
-        print ('fail exception occured')
-        return 'fail exception occured'
+    #try:
+    nu=None
+    exists=User_reg.objects.filter(phone_number=mdict['phone_number']).count()
+    if exists!=0:
+        nu=User_reg.objects.get(phone_number=mdict['phone_number'])
+        if nu.isVerified:
+            return 'fail phone already exists'
+    exists=User_reg.objects.filter(PAN=mdict['PAN']).count()
+    if exists!=0:
+        nu=User_reg.objects.get(PAN=mdict['PAN'])
+        if nu.isVerified:
+            return 'fail user already exists'
+    if nu is None:
+        nu=User_reg()
+    nu.name=mdict['name']
+    nu.phone_number=mdict['phone_number']
+    nu.role=int(mdict['role'])
+    nu.PAN=mdict['PAN']
+    nu.address=mdict['address']
+    if nu.role==1:# is Farmer
+        pass
+    elif nu.role==2:
+        nu.vehicle_model=int(mdict['vehicle_model'])
+        nu.vehicle_capacity=int(mdict['vehicle_capacity'])
+        nu.vehicle_number=mdict['vehicle_number']
+        nu.licence_number=mdict['licence_number']
+    elif nu.role==3:
+        nu.organisation_name=mdict['organisation_name']
+    elif nu.role==4:
+        nu.bank_account_number=mdict['bank_account_number']
+        nu.GST_number=mdict['GST_number']
+    else:
+        raise ('invalid role')
+    nu.isVerified=False
+    otp=str(uuid.uuid1().int%1000000)
+    print('otp is '+otp)
+    client.set(mdict['PAN'],otp,30)
+    print(client.get(mdict['PAN']))       
+    nu.save()
+    return 'success'
+    #except:
+     #   print ('fail exception occured')
+     #   return 'fail exception occured'
 def VerifyUser(mdict):
     client = base.Client(('localhost', 11211))
-    otp_sent=client.get(mdict['PAN'])
+    otp_sent=client.get(mdict['PAN']).decode()
     if otp_sent==None:
         print('timeout')
         return 'timeout'
     otp_recv=mdict['OTP']
+    print(type(otp_sent))
+    print(type(otp_recv))
     if otp_recv != otp_sent:
         print('wrong otp')
         return ('wrong otp')
@@ -137,10 +142,9 @@ def VerifyUser(mdict):
         nu.save()
         print('verification successful')
         return ('verification successful')
+
 suppliers=[]
 categ=None
-
-
 
 
 def GiveResponse(msg,number):

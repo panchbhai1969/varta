e5be8f18de3add1c317a985d150d1ed5a76680f2
Rishabh Poddar <rishabh.poddar0207@gmail.com>
2019-03-02 09:14:29 +0530

    initial image support

--- JioKisan/JioKisan/settings.py
+++ JioKisan/JioKisan/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL= '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'JioKisan/fe_images')
 
 PAYTM_MERCHANT_KEY = "AaQBvILsmgNdgb4h"
 PAYTM_MERCHANT_ID = "sNUXVx29909317083112"
